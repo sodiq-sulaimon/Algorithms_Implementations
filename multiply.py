5dcb4ad14ced22b9fe565308dc4cf718c38fcadc
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2025-02-17 07:33:18 -0600

    Modified the implementation to use floor division symbol '//' instead of math.

--- multiply.py
+++ multiply.py
@@ -1,12 +1,10 @@
-import math
-
 def multiply(x, y):
     if y == 0:
         return 0
-    z = multiply(x, math.floor(y/2))
+    z = multiply(x, y // 2) # y // 2 is floor division
     if y % 2 == 0:
         return 2*z
     else:
         return x + 2*z
     
-print(multiply(8, 7))
+print(multiply(8, 5))
