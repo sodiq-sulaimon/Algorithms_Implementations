b8ca73503d928a3fc4f9fcbcaa136a46f7a91436
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2025-02-17 08:02:36 -0600

    reformatted for readability

--- multiply.py
+++ multiply.py
@@ -8,8 +8,8 @@ def multiply(x, y):
         return 0
     z = multiply(x, y // 2) # y // 2 is floor division
     if y % 2 == 0:
-        return 2*z
+        return 2 * z
     else:
-        return x + 2*z
+        return x + (2 * z)
     
 print(multiply(3, 5))
